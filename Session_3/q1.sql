c0545ef2d73c8c31fd14fcc02879cfb1f35f2cab
Sanampreet Singh <106900639+SanampreetSingh@users.noreply.github.com>
2025-12-14 18:18:33 +0530

    Add files via upload

---
+++ Session_3/q1.sql
@@ -0,0 +1,10 @@
+-- G1_23BCS13053_Sanampreet Singh_
+SELECT s.user_id, ROUND(COALESCE(temp.rate, 0.00), 2) AS confirmation_rate
+FROM Signups AS s
+LEFT JOIN (
+    SELECT user_id, SUM(CASE WHEN action = 'confirmed' THEN 1 ELSE 0 END) / COUNT(*) AS rate
+    FROM Confirmations
+    GROUP BY user_id
+) AS temp
+ON s.user_id = temp.user_id
+ORDER BY confirmation_rate;
